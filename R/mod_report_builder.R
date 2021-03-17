754870a3f72d9a33c8faba541042f34ca6f6cee3
Chris Beeley <chris.beeley@gmail.com>
2021-03-17 19:53:29 +0000

    One more try...?

--- R/mod_report_builder.R
+++ R/mod_report_builder.R
@@ -38,7 +38,8 @@ mod_report_builder_server <- function(id, filter_sentiment, filter_data,
           system.file("app/www/", "report.Rmd",
                       package = "experiencesdashboard"), 
           output_format = "word_document",
-          output_file = "report.docx", 
+          output_file = system.file("app/www/", "report.docx",
+                                    package = "experiencesdashboard"),
           quiet = TRUE, params = params,
           envir = new.env(parent = globalenv())
         )
