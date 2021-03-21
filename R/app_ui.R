0fe36dc85b1bb0fcbfe35eacfc2b47a4ffabf389
Chris Beeley <chris.beeley@gmail.com>
2021-03-21 12:38:01 +0000

    Minor code tidy

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,80 +9,75 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
-    tagList(
-      # Leave this function for adding external resources
-      golem_add_external_resources(),
-      # List the first level UI elements here
-      dashboardPage(
-        
-        dashboardHeader(title = "Patient experience",
-                        titleWidth = 300),
-        dashboardSidebar(
-          width = 300,
-          sidebarMenu(
-            
-            menuItem("Summary", 
-                     tabName = "overview",
-                     icon = shiny::icon("dashboard")
-                     # , badgeLabel = "planned", badgeColor = "orange"
-            ),
-            
-            menuItem("Patient experience", 
-                     tabName = "experiences-user",
-                     icon = shiny::icon("comment"),
-                     selected = TRUE,
-                     badgeLabel = "dev", badgeColor = "green"),
-            
-            # red, yellow, aqua, blue, light-blue, green, navy, teal, 
-            # olive, lime, orange, fuchsia, purple, maroon, black
-            
-            menuItem("Staff experience", 
-                     tabName = "experiences-staff",
-                     icon = shiny::icon("comment-medical"),
-                     badgeLabel = "planned", badgeColor = "orange"
-            ),
-            
-            menuItem("Info", tabName = "info", 
-                     icon = icon("info-circle"), 
-                     menuItem("Data", tabName = "info-data")
-            ),
-            uiOutput("filter_dataUI")
-          )
-        ),
-        dashboardBody(
-          ### Changing theme
-          dashboardthemes::shinyDashboardThemes(
-            theme = "grey_light"
+    # List the first level UI elements here
+    dashboardPage(
+      
+      dashboardHeader(title = "Patient experience",
+                      titleWidth = 300),
+      dashboardSidebar(
+        width = 300,
+        sidebarMenu(
+          
+          menuItem("Summary", 
+                   tabName = "overview",
+                   icon = shiny::icon("dashboard")
+                   # , badgeLabel = "planned", badgeColor = "orange"
           ),
           
-          tabItems(
-            # First tab content
-            tabItem(tabName = "overview",
-                    h1("Overview"),
-                    h2("NOTE: WORK IN PROGRESS"),
-                    p("This tab will contain information that summarises all available data- 
+          menuItem("Patient experience", 
+                   tabName = "experiences-user",
+                   icon = shiny::icon("comment"),
+                   selected = TRUE,
+                   badgeLabel = "dev", badgeColor = "green"),
+          
+          # red, yellow, aqua, blue, light-blue, green, navy, teal, 
+          # olive, lime, orange, fuchsia, purple, maroon, black
+          
+          menuItem("Staff experience", 
+                   tabName = "experiences-staff",
+                   icon = shiny::icon("comment-medical"),
+                   badgeLabel = "planned", badgeColor = "orange"
+          ),
+          
+          menuItem("Info", tabName = "info", 
+                   icon = icon("info-circle"), 
+                   menuItem("Data", tabName = "info-data")
+          ),
+          uiOutput("filter_dataUI")
+        )
+      ),
+      dashboardBody(
+        ### Changing theme
+        dashboardthemes::shinyDashboardThemes(
+          theme = "grey_light"
+        ),
+        
+        tabItems(
+          # First tab content
+          tabItem(tabName = "overview",
+                  h1("Overview"),
+                  h2("NOTE: WORK IN PROGRESS"),
+                  p("This tab will contain information that summarises all available data- 
                     staff and patient experience, clinical outcomes, and risk")
-                    
-            ),
-            
-            tabItem(tabName = "experiences-user",
-                    # h1("Service User Experiences"),
-                    mod_patient_experience_ui("patient_experience_ui_1")
-            ),
-            
-            tabItem(tabName = "experiences-staff",
-                    h1("Staff experiences"),
-                    h2("NOTE: WORK IN PROGRESS")
-            ),
-            
-            tabItem(tabName = "info",
-                    h1("Further Information"),
-                    h2("NOTE: WORK IN PROGRESS"),
-                    p("This tab will contain information about the measures used in the 
+                  
+          ),
+          
+          tabItem(tabName = "experiences-user",
+                  # h1("Service User Experiences"),
+                  mod_patient_experience_ui("patient_experience_ui_1")
+          ),
+          
+          tabItem(tabName = "experiences-staff",
+                  h1("Staff experiences"),
+                  h2("NOTE: WORK IN PROGRESS")
+          ),
+          
+          tabItem(tabName = "info",
+                  h1("Further Information"),
+                  h2("NOTE: WORK IN PROGRESS"),
+                  p("This tab will contain information about the measures used in the 
                     dashboard, as well as other relevant information")
-                    
-            )
+                  
           )
         )
       )
