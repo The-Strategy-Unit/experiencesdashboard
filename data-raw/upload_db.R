e7824187c5457222844f10100fcedc9f3594bb98
Chris Beeley <chris.beeley@gmail.com>
2021-04-22 13:54:37 +0100

    Update Trust C

--- data-raw/upload_db.R
+++ data-raw/upload_db.R
@@ -11,6 +11,13 @@ new_codes <- dplyr::tbl(con, dbplyr::in_schema("SUCE", "NewCodes")) %>%
   collect() %>% 
   mutate(code = tolower(code))
 
+# function to produce probabilities for sampling
+
+make_prob <- function(values){
+  
+  values / sum(values)
+}
+
 # trust b----
 
 trust_b <- read_excel(
@@ -48,12 +55,11 @@ trust_b <- read_excel(
              code == code1_neg ~ crit_neg * -1,
              TRUE ~ crit_pos
            ),
-         criticality =
-           case_when(
-             criticality > 5 ~ NA_real_,
-             criticality < -5 ~ NA_real_,
-             TRUE ~ criticality
-           ),
+         criticality = case_when(
+           criticality > 5 ~ NA_real_,
+           criticality < -5 ~ NA_real_,
+           TRUE ~ criticality
+         ),
          criticality = case_when(
            code == "xn" ~ abs(criticality),
            TRUE ~ criticality
@@ -63,10 +69,56 @@ trust_b <- read_excel(
   select(location_1 : location_3, fft, comment, code, criticality, patient_carer) %>% 
   left_join(new_codes, by = "code") %>% 
   mutate(across(starts_with("location"), ~ nottshcMethods::hash(.x, n_char = 8))) %>% 
-  mutate(fft = sample(c(1, 2, 3, 3, 3, rep(NA, 5),
-                        rep(4, 7), rep(5, 15)), n(), replace = TRUE))
-
+  mutate(fft = sample(c(NA, 1 : 5), n(), replace = TRUE, 
+                      prob = make_prob(c(1, 2, 3, 4, 7, 9))))
 
 # trust c----
 
-trust_c <- read_xlsx("data-raw/TrustC.xlsx")
+trust_c <- read_xlsx("data-raw/TrustC.xlsx") %>%
+  select(location_1 = Directorate, location_2 = Specialty, 
+         location_3 = LocationName, date = InputDate,
+         comment = Comment, code = `Code 1`,
+         criticality = Serverity) %>% 
+  mutate(
+    criticality = as.numeric(criticality),
+    code = tolower(code)) %>%
+  mutate(fft = sample(c(NA, 1 : 5), n(), replace = TRUE, 
+                      prob = make_prob(c(1, 2, 3, 4, 7, 9))),
+         criticality = case_when(
+           criticality > 5 ~ NA_real_,
+           criticality < -5 ~ NA_real_,
+           TRUE ~ criticality
+         )) %>% 
+  mutate(across(starts_with("location"), ~ nottshcMethods::hash(.x, n_char = 8))) %>% 
+  left_join(new_codes, by = "code")
+
+# demographics----
+
+# gender
+
+c("Male", "Female", "Other")
+
+# age
+
+c("Up to 25", "26 - 35", "36 - 45", "46 - 55", "56 - 65", "65-74", 
+  "75-84", "85-94", "95 plus")
+
+# ethnicity
+
+c("Asian", "Black / African / Caribbean", "Mixed / Multiple ethnic group", 
+  "Other ethnic group", "White", "Not recorded")
+
+# sexuality
+
+c("Bisexual", "Gay man", "Gay woman / lesbian", "Heterosexual", 
+  "Heterosexual / straight", "Other", "Prefer not to say", 
+  "Prefer to self-describe:", "Not recorded")
+
+# patient/ carer
+
+c("Carer", "Parent", "Patient", "Not recorded")
+
+# disability
+
+c("No", "Not limited", "Yes, limited a little", "Yes, limited a lot", 
+  "Prefer not to say", "Not recorded")
