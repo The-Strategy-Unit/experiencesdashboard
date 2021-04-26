75e603b1f38b77db9a0880e047f609d496cd69f4
Chris Beeley <chris.beeley@gmail.com>
2021-04-26 11:44:14 +0100

    Add test to fix encoding problems

--- data-raw/data_raw.R
+++ data-raw/data_raw.R
@@ -21,7 +21,22 @@ trust_a <- tidy_px_exp(open_db_data) %>%
 trust_a <- trust_a %>% 
   mutate(across(gender : baby, ~ sample(.x, n())))
 
-usethis::use_data(trust_a, overwrite = TRUE)
+# test because of encoding problems
+
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = "MySQL ODBC 8.0 Unicode Driver",
+                      Server   = Sys.getenv("HOST_NAME"),
+                      UID      = Sys.getenv("DB_USER"),
+                      PWD      = Sys.getenv("MYSQL_PASSWORD"),
+                      Port     = 3306,
+                      database = "SUCE",
+                      encoding = "UTF-8")
+
+df <- data.frame(x = 1, y = "佃煮惣菜", z = "It's okay", a = "Zürich")
+dbWriteTable(con, 'test-utf8', df, temporary = TRUE)
+dbReadTable(con, 'test-utf8')
+
+dbDisconnect(con)
 
 # ## Load results from Andreas ----
 # sentiment_txt_data <- readRDS(file = here::here("data-raw/sentiment_txt_data.rds")) %>% 
