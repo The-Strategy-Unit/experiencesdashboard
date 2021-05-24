9628afd2617b57f1731be151088fbf8077d88561
Chris Beeley <chris.beeley@gmail.com>
2021-05-24 18:01:24 +0100

    Fix for comment labels

--- R/mod_category_criticality.R
+++ R/mod_category_criticality.R
@@ -26,12 +26,16 @@ mod_category_criticality_ui <- function(id){
         type = "tabs",
         tabPanel("Summary", value = "summary",
                  fluidRow(
-                   column(6, h2("What could we improve?"),
+                   column(6, h2(get_golem_config("comment_1")),
                           p("Click a row to see comments related to that category"),
                           mod_click_tables_ui("click_tables_ui_1")),
-                   column(6, h2("What did we do well?"),
-                          p("Click a row to see comments related to that category"),
-                          mod_click_tables_ui("click_tables_ui_2"))
+                   
+                   if(isTruthy(get_golem_config("comment_2"))){
+                     
+                     column(6, h2(get_golem_config("comment_2")),
+                            p("Click a row to see comments related to that category"),
+                            mod_click_tables_ui("click_tables_ui_2"))
+                   }
                  )
         ),
         tabPanel("Comments", value = "comments",
