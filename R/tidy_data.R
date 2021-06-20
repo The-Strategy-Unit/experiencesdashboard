932b832055263491b7aaefa113666e95463679b0
Chris Beeley <chris.beeley@gmail.com>
2021-06-20 13:13:15 +0100

    Fix capitalisation of comment codes

--- R/tidy_data.R
+++ R/tidy_data.R
@@ -68,7 +68,7 @@ tidy_all_trusts <- function(data, conn, trust_id = "trust_a") {
   if(trust_id == "trust_a"){
     
     db_tidy <- db_tidy %>%
-      dplyr::mutate_at(dplyr::all_of(code_fields), toupper) %>%
+      dplyr::mutate(code = toupper(code)) %>%
       dplyr::left_join(db_codes_exp_data,
                        by = c("code"), copy = TRUE) %>%
       dplyr::mutate(crit = case_when(comment_type == "comment_1" ~ imp_crit * -1,
