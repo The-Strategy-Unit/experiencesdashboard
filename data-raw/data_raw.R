4f835340e074baa0357e63f8cb31a206645a23d7
Chris Beeley <chris.beeley@gmail.com>
2021-08-15 23:20:37 +0100

    Update for trust a

--- data-raw/data_raw.R
+++ data-raw/data_raw.R
@@ -2,13 +2,13 @@
 
 ## Load packages ----
 library(tidyverse)
-library(nottshc)
+library(nottshcData)
 library(nottshcMethods)
 library(DBI)
 
 ## MySQL ----
 
-open_db_data <- get_px_exp(from = "2020-10-01",
+open_db_data <- get_px_exp(from = "2015-01-01",
                            open_data = FALSE, 
                            remove_demographics = FALSE,
                            remove_optout = TRUE)
@@ -17,10 +17,63 @@ trust_a <- open_db_data %>%
   arrange(Date) %>% 
   collect()
 
+# final dataset needs location_1, location_2 location_3, date, 
+# comment_txt, comment_type, fft, gender, age, ethnicity,
+# sexuality, patient_carer, disability, faith
+
+trust_a <- trust_a %>% 
+  dplyr::select(location_1 = Division2, location_2 = DirT, location_3 = TeamN,
+                date = Date, Improve, Best, fft = Service, gender = Gender, age = Age,
+                ethnicity = Ethnic, sexuality = Sexuality, patient_carer = SU,
+                disability = Disability, faith = Religion) %>% 
+  tidyr::pivot_longer(cols = c(Improve, Best),
+                      names_to = "comment_type",
+                      values_to = "comment_txt") %>% 
+  clean_dataframe(., comment_txt)
+
 # MUST randomise the demographic features
 
 trust_a <- trust_a %>% 
-  mutate(across(Gender : Baby, ~ sample(.x, n())))
+  dplyr::mutate(dplyr::across(c(gender, age, ethnicity, sexuality, 
+                                patient_carer, disability, faith), 
+                              ~ sample(.x, dplyr::n())))
+
+preds <- experienceAnalysis::calc_predict_unlabelled_text(
+  x = trust_a,
+  python_setup = FALSE,
+  text_col_name = 'comment_txt',
+  preds_column = NULL,
+  column_names = "all_cols",
+  pipe_path = 'fitted_pipeline.sav'
+) %>% 
+  dplyr::select(code = comment_txt_preds)
+
+criticality <- experienceAnalysis::calc_predict_unlabelled_text(
+  x = trust_a,
+  python_setup = FALSE,
+  text_col_name = 'comment_txt',
+  preds_column = NULL,
+  column_names = "all_cols",
+  pipe_path = 'pipeline_criticality.sav'
+) %>% 
+  dplyr::select(crit = comment_txt_preds)
+
+final_df <- dplyr::bind_cols(
+  trust_a, 
+  preds,
+  criticality)
+
+# blank comments should give blank predictions
+
+final_df$code[is.na(final_df$comment_txt)] = NA
+final_df$crit[is.na(final_df$comment_txt)] = NA
+
+final_df <- final_df %>% 
+  dplyr::mutate(age = dplyr::recode(
+    age, `1` = "Under 12", `2` = "12-17", `3` = "18-25", `4` = "26-39",
+    `5` = "40-64", `6` = "65-79", `7` = "80+", .default = NA_character_))
+
+# add code and crit
 
 con <- DBI::dbConnect(odbc::odbc(),
                       Driver   = "Maria DB",
@@ -31,22 +84,6 @@ con <- DBI::dbConnect(odbc::odbc(),
                       database = "TEXT_MINING",
                       encoding = "UTF-8")
 
-dbWriteTable(con, 'trust_a', trust_a, overwrite = TRUE)
+dbWriteTable(con, 'trust_a', final_df, overwrite = TRUE)
 
 dbDisconnect(con)
-
-## Load results from Andreas ----
-sentiment_txt_data <- readRDS(file = here::here("data-raw/sentiment_txt_data.rds")) %>%
-  dplyr::rename(division = division2, directorate = directorate2)
-
-# add fake dates
-
-sentiment_txt_data$date <- sample(tidy_trust_data$date,
-                                  nrow(sentiment_txt_data), replace = TRUE)
-
-sentiment_txt_data <- sentiment_txt_data %>%
-  dplyr::mutate(division = dplyr::recode(division,
-                                         "Local partnerships- CH" = "General health services",
-                                         "Forensic services" = "Forensic"))
-
-usethis::use_data(sentiment_txt_data, overwrite = TRUE)
