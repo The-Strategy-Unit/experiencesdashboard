b6c60ac82d243d7c0e79c5dd2eae27d5285d1281
asegun-cod <asegun20@gmail.com>
2023-01-23 14:32:48 +0000

    resolve issues with the search function

--- R/filter_text.R
+++ R/filter_text.R
@@ -1,60 +1,37 @@
 #' Return text from a freetext search
-#' @description combine search terms with OR and return text from a specific
+#' @description combine search terms with OR and AND then return text from a specific
 #' question
 #' @param text_data the dataframe, raw from the database
 #' @param filter_text comma separated string with search terms in
 #' @param comment_type_filter which comment to return- 1 or 2
 #' @param search_type type of search ('and', 'or')
-#' 
+#'
 #' @export
-#' @return string vector of search terms, separated by <p>, </p> for 
+#' @return string vector of search terms, separated by <p>, </p> for
 #' display as raw HTML by Shiny
 
-return_search_text <- function(text_data, filter_text, comment_type_filter, search_type= c('or','and')){
-  # remove trailing punctuation from both input strings
-  search_string <- filter_text
+return_search_text <- function(text_data, filter_text, comment_type_filter, search_type = c("or", "and")) {
+  # split on commas and remove trailing punctuation from both input strings
+  search_strings <- strsplit(filter_text, ",")[[1]] %>%
+    sub("[^[:alpha:]+]$", "", .) %>%
+    tolower()
 
-  searchTextInclude <- sub("[[:punct:]]$", "", trimws(search_string))
+  # check argument is valid and choose the correct logical predicate
+  search_type <- match.arg(search_type)
+  stopifnot("search type must be one of 'or', or 'and'" = length(search_type) == 1)
+  search_fn <- switch(search_type,
+    "or" = any,
+    "and" = all
+  )
 
-  text_data <- text_data %>%
-    dplyr::filter(comment_type == comment_type_filter)
+  comments <- text_data %>%
+    dplyr::filter(comment_type == comment_type_filter) %>%
+    dplyr::pull(comment_txt)
 
-  if (search_type == 'and'){
-    lt = text_data %>% dplyr::pull(comment_txt)
+  matched_comments <- comments %>%
+    tolower() %>%
+    strsplit(" ") %>%
+    vapply(\(x) search_fn(search_strings %in% x), logical(1))
 
-    for (i in 1:length(searchTextInclude)){
-      search_result = text_data %>%
-        dplyr::filter(grepl(searchTextInclude[i], comment_txt, ignore.case = TRUE)) %>%
-        dplyr::pull(comment_txt)
-      lt = Reduce(intersect, list(lt,search_result))
-    }
-    lt %>% paste0("<p>", ., "</p>")
-  }
-  else{
-    pattern <- paste(trimws(unlist(strsplit(searchTextInclude, ","))), collapse = "|")
-    text_data %>%
-      dplyr::filter(
-        grepl(pattern, comment_txt, ignore.case = TRUE)
-      ) %>%
-      dplyr::pull(comment_txt) %>% 
-      paste0("<p>", ., "</p>")
-  }
+  paste0("<p>", comments[matched_comments], "</p>")
 }
-
-# return_search_text <- function(text_data, filter_text, comment_type_filter){
-#   # remove trailing punctuation from both input strings
-#   
-#   search_string <- filter_text
-#   
-#   searchTextInclude <- sub("[[:punct:]]$", "", trimws(search_string))
-#   
-#   text_data %>%
-#     dplyr::filter(comment_type == comment_type_filter) %>% 
-#     dplyr::filter(grepl(paste(
-#       trimws(unlist(strsplit(searchTextInclude, ","))), 
-#       collapse = "|"), comment_txt)) %>% 
-#     dplyr::pull(comment_txt) %>%
-#     paste0("<p>", ., "</p>")
-#   
-# }
-
