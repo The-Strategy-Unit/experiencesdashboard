6f2039a4f897a6d1218e1da978746b66491a0b44
asegun-cod <asegun20@gmail.com>
2023-02-24 18:17:15 +0000

    add horizontal line

--- R/show_text.R
+++ R/show_text.R
@@ -5,6 +5,7 @@ show_text <- function(data, filter_by_column, filter_by_text, comment_type_filte
       dplyr::filter(comment_type == comment_type_filter,
                     .data[[filter_by_column]] == filter_by_text) %>% 
       dplyr::pull(comment_txt) %>%
-      paste0("<p>", ., "</p>")
+      # paste0("<p>", ., "</p>",hr())
+      paste0(.,hr())
   )
 }
