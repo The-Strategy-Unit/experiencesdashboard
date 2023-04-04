ba6f5873255d03b4d223ceb22c4ddd201aa19c6b
asegun-cod <asegun20@gmail.com>
2023-04-04 17:11:39 +0100

    populates summaey tab with fft and report builder contents

--- R/mod_summary.R
+++ R/mod_summary.R
@@ -9,12 +9,8 @@
 #' @importFrom shiny NS tagList
 mod_summary_ui <- function(id) {
   ns <- NS(id)
-  tagList(
-    fluidPage(
-      h1("Overview"),
-      mod_summary_record_ui("summary_record_1")
-    )
-  )
+  
+  uiOutput(ns("dynamic_summary"))
 }
 
 #' summary Server Functions
@@ -23,5 +19,32 @@ mod_summary_ui <- function(id) {
 mod_summary_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+    
+    output$dynamic_summary <- renderUI({
+      
+      tagList(
+          h1("Overview"),
+          mod_summary_record_ui("summary_record_1"),
+          
+          tags$hr(),
+          
+          fluidRow(
+            
+            # Report builder
+            
+            column(4, mod_report_builder_ui("report_builder_ui_1")),
+            
+            # FFT tab (do we have FFT data?)
+            
+            if(isTruthy(get_golem_config("question_1"))){
+              column(8, mod_fft_ui("fft_ui_1"))
+            } else{
+                column(8)
+            }
+          ),
+          
+          tags$hr()
+        )
+      })
   })
 }
