1966cf5ece590516058cdd893ece92a4d400476c
asegun-cod <asegun20@gmail.com>
2023-04-05 22:17:56 +0100

    add box to outputs

--- R/mod_summary.R
+++ R/mod_summary.R
@@ -9,7 +9,7 @@
 #' @importFrom shiny NS tagList
 mod_summary_ui <- function(id) {
   ns <- NS(id)
-  
+
   uiOutput(ns("dynamic_summary"))
 }
 
@@ -19,32 +19,37 @@ mod_summary_ui <- function(id) {
 mod_summary_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    
+
     output$dynamic_summary <- renderUI({
-      
       tagList(
-          h1("Overview"),
-          mod_summary_record_ui("summary_record_1"),
+        br(),
+        
+        mod_summary_record_ui("summary_record_1"),
+        
+        hr(),
+        
+        fluidRow(
+
+          # FFT tab (do we have FFT data?)
           
-          tags$hr(),
+          if (isTruthy(get_golem_config("question_1"))) {
+            column(8,
+                   box(
+                     mod_fft_ui("fft_ui_1"),
+                     title = "FFT Score", status = "primary", solidHeader = TRUE, width = NULL
+                   ))
+          },
+
+          # Report builder
           
-          fluidRow(
-            
-            # Report builder
-            
-            column(4, mod_report_builder_ui("report_builder_ui_1")),
-            
-            # FFT tab (do we have FFT data?)
-            
-            if(isTruthy(get_golem_config("question_1"))){
-              column(8, mod_fft_ui("fft_ui_1"))
-            } else{
-                column(8)
-            }
-          ),
-          
-          tags$hr()
-        )
-      })
+          column(
+            4,
+            box(mod_report_builder_ui("report_builder_ui_1"),
+                title = "Report builder", status = "primary", solidHeader = TRUE, width = NULL
+            )
+          )
+        ),
+      )
+    })
   })
 }
