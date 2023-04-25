0262c19cdaa3fb884646d975283650ebed1bbade
asegun-cod <asegun20@gmail.com>
2023-04-25 16:39:23 +0100

    add nhs color to dashboard theme

---
+++ R/fct_nhs_shiny_theme.R
@@ -0,0 +1,19 @@
+#' nhs_shiny_theme
+#'
+#' @description Create a shiny theme with NHS blue color
+#'
+#' @return a string value of the compiled CSS
+#'
+#' #' @noRd
+nhs_shiny_theme <- function() {
+  fresh::create_theme(
+    fresh::adminlte_color(
+      light_blue = "#005EB8"
+    ),
+    fresh::adminlte_sidebar(
+      width = "250px",
+      dark_hover_bg = "#41B6E6",
+      dark_bg = "#D8DEE9",
+    )
+  )
+}
