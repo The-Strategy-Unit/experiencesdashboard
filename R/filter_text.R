1e5a31052c330fd552a697d28804bbe8922e81a2
asegun-cod <asegun20@gmail.com>
2023-06-03 04:23:11 +0100

    fix bug in function

--- R/filter_text.R
+++ R/filter_text.R
@@ -9,14 +9,11 @@
 filter_df <- function(text_data, comment_type_filter = NULL) {
   
   if(is.null(comment_type_filter)){
-    text_data %>%
-      dplyr::pull(comment_txt)
-    
+    text_data 
   } else{
     
     text_data %>%
-      dplyr::filter(comment_type == comment_type_filter) %>%
-      dplyr::pull(comment_txt)
+      dplyr::filter(comment_type == comment_type_filter)
   }
 }
 
@@ -53,10 +50,7 @@ lowered_comments <- function(comments) {
 #' @param string a string
 #' @param search_strings list of strings with search terms in it
 #' @param search_fn type of search ('and', 'or')
-#'
 #' @noRd
-#'
-#' @return
 check_match <- function(string, search_strings, search_fn) {
   search_fn(
     lapply(
@@ -66,7 +60,7 @@ check_match <- function(string, search_strings, search_fn) {
   )
 }
 
-#' Find index in a list of string where search word(s) exist
+#' Find comment in list of comments where all/any of search strings exist
 #'
 #' @param lowered_comments list of strings in lowercase
 #' @param search_fn type of search type ('and', 'or')
@@ -78,7 +72,7 @@ check_match <- function(string, search_strings, search_fn) {
 #' matched_comments(
 #'   lowered_comments = c(
 #'     "tricky times, I recommend quick appraisals ",
-#'     "time of my situation.",
+#'     "time of my situation is quick",
 #'     ">quick response time, competent appraisal of my situation"
 #'   ),
 #'   search_fn = all, search_strings = c("quick", "time", "appraisal")
@@ -95,11 +89,11 @@ matched_comments <- function(lowered_comments, search_fn, search_strings) {
 sanitized_search_strings <- function(filter_text){
   
   sanitized_input <- input_sanitizer(filter_text) 
-  stemmed_input = tm::stemDocument(sanitized_input)
-  search_strings <- union(sanitized_input, stemmed_input)
+  stemmed_input <- tm::stemDocument(sanitized_input)
+  search_strings <- union(sanitized_input, stemmed_input) 
+  search_strings <- search_strings[search_strings!='']
   
   return(search_strings)
-  cat("search strings: ") # for logging
 }
 
 #' Return text from a freetext search
@@ -113,17 +107,19 @@ sanitized_search_strings <- function(filter_text){
 #' @export
 #' @return string vector of search terms, separated by <p>, </p> for
 #' display as raw HTML by Shiny
-#' @example
+#' @examples
 #' return_search_text(
-#' text_data = data.frame(comment_txt = c("tricky times, I recommend quick appraisals ",
-#' "time of my situation.", ">quick response time, competent appraisal of my situation"),
-#' comment_type = c('comment_1', 'comment_1', 'comment_1')),
-#' filter_text = 'Uick, time, appraisal$, &!',
-#' comment_type_filter='comment_1', search_type = "and")
+#'  text_data = data.frame(comment_txt = c("tricky times, I recommend quick appraisals ",
+#'  "time of my situation.", ">quick response time, competent appraisal of my situation"),
+#'  comment_type = c('comment_1', 'comment_1', 'comment_1')),
+#'  filter_text = 'Uick, time, appraisal$, &!',
+#'  comment_type_filter='comment_1', search_type = "and",
+#'  return_dataframe = FALSE
+#' )
 return_search_text <- function(text_data, filter_text, comment_type_filter = NULL,
                                search_type = c("or", "and"), return_dataframe = TRUE) {
   # check argument is valid and choose the correct logical predicate
-  search_type <- match.arg(search_type)
+  search_type <- match.arg(search_type, c("or", "and"))
   stopifnot("search type must be one of 'or', or 'and'" = length(search_type) == 1)
   search_fn <- switch(search_type,
     "or" = any,
@@ -135,7 +131,10 @@ return_search_text <- function(text_data, filter_text, comment_type_filter = NUL
   cat("search strings: ") # for logging
   print(search_strings) # for logging
 
-  comments <- filter_df(text_data, comment_type_filter)
+  text_data <- filter_df(text_data, comment_type_filter)
+  
+  comments <- text_data %>%
+    dplyr::pull(comment_txt)
 
   matched_rows <- comments %>%
     lowered_comments() %>%
