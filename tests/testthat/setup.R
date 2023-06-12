0a7c98e8f987fed8bd3b86b56acec0ab54bea01b
asegun-cod <asegun20@gmail.com>
2023-06-12 10:40:19 +0100

    update test setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,6 @@
 library(data.validator)
 library(assertr)
 library(dplyr)
-load(here::here("tests/framework.rda"))
 
 test <- readr::read_csv(here::here("tests/test_data.csv"),
   show_col_types = FALSE
@@ -10,20 +9,10 @@ test <- readr::read_csv(here::here("tests/test_data.csv"),
 tidy_trust_data <- test %>%
   tidy_all_trusts()
 
-# data base data
-pool <- odbc::dbConnect(
-  drv = odbc::odbc(),
-  driver = Sys.getenv("odbc_driver"),
-  server = Sys.getenv("HOST_NAME"),
-  UID = Sys.getenv("DB_USER"),
-  PWD = Sys.getenv("MYSQL_PASSWORD"),
-  database = "TEXT_MINING",
-  Port = 3306
-)
-
-# read data that mimic the data base
+# read data that mimic the data template Trusts will use to upload their data
 phase_2_upload_data <- readRDS(here::here("tests/phase_2_test_template.rds"))
 
+# read data that mimic the database data
 phase_2_db_data <- readRDS(here::here("tests/p2_db_data_template.rds")) %>%
   dplyr::mutate(across(c(category, super_category), ~ purrr::map(.x, rawToChar)))
 
