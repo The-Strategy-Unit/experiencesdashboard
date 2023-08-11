f0673c51299e05f13200ce5f336b87d16a104766
asegun-cod <asegun20@gmail.com>
2023-08-11 15:53:18 +0100

    create new function for the new API implementation

--- tests/testthat/test-fct_api_pred.R
+++ tests/testthat/test-fct_api_pred.R
@@ -11,31 +11,55 @@ test_that("API and Framework are in sync", {
   expect_true(all(not_in_framewk == c("Labelling not possible", "Admission")))
 })
 
-test_that("API prediction function is working...", {
-  text_data <- data.frame(
-    comment_id = c(1, 2, 3),
-    comment_text = c(
-      "Food was cold", "Those satffs are just too amazing",
-      "I hate the meal"
-    ),
-    question_type = c("what_good", "could_improve", "nonspecific")
+# test_that("API prediction function is working...", {
+#   text_data <- data.frame(
+#     comment_id = c(1, 2, 3),
+#     comment_text = c(
+#       "Food was cold", "Those satffs are just too amazing",
+#       "I hate the meal"
+#     ),
+#     question_type = c("what_good", "could_improve", "nonspecific")
+#   )
+# 
+#   expect_error(api_pred(text_data |>
+#     select(-question_type)))
+# 
+#   expect_no_error(api_pred(text_data))
+# 
+#   preds <- text_data |>
+#     batch_predict()
+# 
+#   expect_equal(nrow(preds), 3)
+#   expect_true(all(c("comment_id", "comment_text", "labels") %in% names(preds)))
+#   expect_equal(sum(is.na(preds$labels)), 0)
+# })
+# 
+# test_that("api_question_code works", {
+#   expect_equal(api_question_code("What did we do well"), "what_good")
+#   expect_equal(api_question_code("What could be improved"), "could_improve")
+#   expect_equal(api_question_code("why that answer"), "nonspecific")
+# })
+
+test_that("api_pred_url works and return expected result", {
+  
+  comment_id <- c("1", "2", "3")
+  comment_text <- c(
+    "Nurse was great but very difficult to find parking",
+    "The ward was freezing.",
+    ""
   )
-
-  expect_error(api_pred(text_data |>
-    select(-question_type)))
-
-  expect_no_error(api_pred(text_data))
-
-  preds <- text_data |>
-    batch_predict()
-
-  expect_equal(nrow(preds), 3)
-  expect_true(all(c("comment_id", "comment_text", "labels") %in% names(preds)))
-  expect_equal(sum(is.na(preds$labels)), 0)
-})
-
-test_that("api_question_code works", {
-  expect_equal(api_question_code("What did we do well"), "what_good")
-  expect_equal(api_question_code("What could be improved"), "could_improve")
-  expect_equal(api_question_code("why that answer"), "nonspecific")
+  question_type <- c("what_good", "could_improve", "nonspecific")
+  df <- data.frame(comment_id, comment_text, question_type)
+  
+  
+  expect_error(api_pred_url(df |>
+                          select(-question_type)))
+  
+  expect_no_error(api_pred_url(df))
+  
+  api_result <- api_pred_url(df)
+  expect_true(inherits(api_result,  c("character", 'data.frame')))
+  
+  expect_no_error(get_pred_from_url(api_result))
+  expect_true(any(inherits(get_pred_from_url(api_result), 'data.frame') | get_pred_from_url(api_result) == 'Busy'))
 })
