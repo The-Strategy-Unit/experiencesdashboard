d4c79c21ad34158029d28de30b19d4c28518ca57
asegun-cod <asegun20@gmail.com>
2023-10-11 18:32:10 +0100

    add guidance for "Making best use of the qualitative comments"

--- R/mod_documentation_page.R
+++ R/mod_documentation_page.R
@@ -8,26 +8,41 @@
 mod_documentation_page_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    br(),
+    h3(strong("Introduction to the Data Categorisation Framework"), style = "color : #005EB8;"),
     p("This dashboard utilizes the pxtextmining API, a machine learning tool, to assign one or more subcategories to free-text comments
       based on the Qualitative Data Categorization (QDC) framework. The QDC framework is an evidence-based work that has been designed
-      with several categories, each with its own set of subcategories. The categories group similar topics together to make it easier 
+      with several categories, each with its own set of subcategories. The categories group similar topics together to make it easier
       for users to navigate the framework, while the subcategories reflect the actual topics that better represent the underlying data."),
-    p("The dashboard's visualizations and intuitive interactivity are thoughtfully created to help users effectively engage with the 
+    p("The dashboard's visualizations and intuitive interactivity are thoughtfully created to help users effectively engage with the
       comments and not merely quantify the data. Below is a high-level visual of the categories and subcategories:"),
     img(src = "www/framework_MVP_version.jpeg", width = "100%"),
-    hr(),
+    rep_br(2),
     p("To see detailed description of the sub-categories, kindly click on the category to expand it."),
     DT::DTOutput(ns("framework_table")),
-    hr(),
     tagList(
-      p("To get further detail about the data categorisation framework and the dashboard
+      h4(strong("Making best use of the qualitative comments"), style = "color : #005EB8;"),
+      HTML(paste(
+        "This dashboard should be used to facilitate initial exploration of your qualitative data, before
+        drawing fuller insight from the underlying qualitative comments. Before using the dashboard, you
+        should read the good practice guidance on the ",
+        a(strong("documentation page"),
+          href = "https://cdu-data-science-team.github.io/PatientExperience-QDC/framework/framework.html",
+          target = "_blank"
+        ),
+        ". This includes important
+        information, tips, and advice to help you maximise your use of the categorised qualitative comments,
+        whilst avoiding the risks around relying on the quantification of qualitative data."
+      )),
+      rep_br(2),
+      HTML(paste(
+        "To get further detail about the data categorisation framework and the dashboard
         including some illustrative examples for each of the sub-categories.
-        Please see the"),
-      a(p("Patient Experience - QDC documentation Page"),
-        href = "https://cdu-data-science-team.github.io/PatientExperience-QDC/framework/framework3.html",
-        target = "_blank"
-      )
+        Please see the",
+        a(strong("Patient Experience - QDC documentation Page."),
+          href = "https://cdu-data-science-team.github.io/PatientExperience-QDC/framework/framework3.html",
+          target = "_blank"
+        )
+      ))
     ),
   )
 }
@@ -40,7 +55,6 @@ mod_documentation_page_server <- function(id) {
 
     # table
     output$framework_table <- DT::renderDT({
-
       # JaveScript code to collapse the table
       callback_js <- DT::JS(
         "table.on('click', 'tr.dtrg-group', function () {",
